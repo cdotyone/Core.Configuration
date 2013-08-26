da26a77eb02196014c30192449445d9ac2382fd1
Chris Doty (Civic) <dotyc@civicinc.com>
2013-08-26 09:18:13 -0500

    more namespace changes

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("Civic.Core.Configuration")]
+[assembly: AssemblyProduct("Civic.Core.Configuration")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51a84bea-81c5-4213-a178-40e24183dd30")]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2012 Civic Engineering & IT")]
