cd342047ae6394bf06e47966f3cc63fb04f10be0
dotyc <dotyc@civicinc.com>
2016-03-22 16:32:27 -0500

    allow auditlog to use local time

---
+++ CivicSection.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace Civic.Core.Configuration
+{
+
+    public class CivicSection : Section
+    {
+        /// <summary>
+        /// The current configuration for the configuration library
+        /// </summary>
+        public static CivicSection Current
+        {
+            get
+            {
+                if (_coreConfig == null) _coreConfig = (CivicSection) ConfigurationManager.GetSection(SectionName);
+                return _coreConfig;
+            }
+        }
+
+        private static CivicSection _coreConfig;
+
+
+        /// <summary>
+        /// The name of the configuration section.
+        /// </summary>
+        public static string SectionName
+        {
+            get { return "civic"; }
+        }
+    }
+}
