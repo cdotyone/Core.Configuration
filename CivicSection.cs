8a8cfab4c9cec828aa261aeb2b02c4009f633660
dotyc <dotyc@civicinc.com>
2016-05-25 23:44:43 -0500

    move appname, EnvironmentCode, ClientCode into main civic section

--- CivicSection.cs
+++ CivicSection.cs
@@ -39,5 +39,33 @@ namespace Civic.Core.Configuration
             get { return (bool)base[Constants.CONFIG_DEVMODE_PROP]; }
             set { base[Constants.CONFIG_DEVMODE_PROP] = value; }
         }
+
+        [ConfigurationProperty(Constants.CONFIG_CLIENTCODE_PROP, IsRequired = false)]
+        public string ClientCode
+        {
+            get { return GetAttribute(Constants.CONFIG_CLIENTCODE_PROP, Constants.CONFIG_CLIENTCODE_DEFAULT); }
+            set { base[Constants.CONFIG_CLIENTCODE_PROP] = value; }
+        }
+
+        [ConfigurationProperty(Constants.CONFIG_ENVCODE_PROP, IsRequired = false)]
+        public string EnvironmentCode
+        {
+            get { return GetAttribute(Constants.CONFIG_ENVCODE_PROP, Constants.CONFIG_ENVCODE_DEFAULT); }
+            set { base[Constants.CONFIG_ENVCODE_PROP] = value; }
+        }
+
+        [ConfigurationProperty(Constants.CONFIG_APPNAME_PROP, IsRequired = false)]
+        public string ApplicationName
+        {
+            get { return GetAttribute(Constants.CONFIG_APPNAME_PROP, Constants.CONFIG_APPNAME_DEFAULT); }
+            set { base[Constants.CONFIG_APPNAME_PROP] = value; }
+        }
+
+        public string GetAttribute(string name, string defaultValue)
+        {
+            if (Attributes.ContainsKey(name)) return Attributes[name];
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings[name])) return defaultValue;
+            return ConfigurationManager.AppSettings[name];
+        }
     }
 }
