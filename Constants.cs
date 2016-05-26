5826fc3196dd7a4aec1e5c6673acbfa25a6129ba
Chris Doty <cdotyone@gmail.com>
2016-05-25 23:44:43 -0500

    move appname, EnvironmentCode, ClientCode into main civic section

--- Constants.cs
+++ Constants.cs
@@ -14,6 +14,16 @@
         public const string CONFIG_DEVMODE_PROP = "devMode";
         public const bool CONFIG_DEVMODE_DEFAULT = false;
 
+
+        public const string CONFIG_CLIENTCODE_PROP = "client";
+        public const string CONFIG_CLIENTCODE_DEFAULT = "CIVIC";
+
+        public const string CONFIG_ENVCODE_PROP = "env";
+        public const string CONFIG_ENVCODE_DEFAULT = "PROD";
+
+        public const string CONFIG_APPNAME_PROP = "appname";
+        public const string CONFIG_APPNAME_DEFAULT = "Unknown";
+
         internal class SR
     	{
     		public const string CONFIG_KEY_NULL = "ConfigHelper_Key_Null";
