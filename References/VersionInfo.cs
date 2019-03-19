7c1489c278b750dc32dc2e38b5fa499079834152
Chris Doty <cdotyone@gmail.com>
2019-03-19 09:29:15 -0500

    ...

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.26.0.0")]
-[assembly: AssemblyFileVersion("1.26.12.6859")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2019 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
