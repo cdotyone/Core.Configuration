63445f72ecfbebcbb9af3f37d3535bde4f659ee5
Chris Doty <cdotyone@gmail.com>
2019-03-19 13:36:11 -0500

    [ci skip]

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.1.47")]
+[assembly: AssemblyFileVersion("2.0.1.50")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
