cd1df5756f297a15d379d67c35bbf393fed3372f
Chris Doty <cdotyone@gmail.com>
2020-07-20 16:31:51 -0500

    chore: update copyright year

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.9.139")]
 [assembly: AssemblyCompany("Ten Codigo")]
-[assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
+[assembly: AssemblyCopyright("Copyright © 2020 Chris Doty")]
